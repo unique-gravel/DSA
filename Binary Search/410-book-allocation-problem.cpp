f440968071a7a2b7f08dc62c6c2c662382cb5b88
Your Name <you@example.com>
2022-11-21 14:33:30 +0530

    comments added to book allocation problem

--- Binary Search/410-book-allocation-problem.cpp
+++ Binary Search/410-book-allocation-problem.cpp
@@ -1,3 +1,5 @@
+//binary search on the solution space
+
 #include <iostream>
 #include<vector>
 using namespace std;
@@ -6,15 +8,19 @@ bool isSolution(vector<int>& arr, int n, int mid, int k) {
     int studentCount = 1;
     int pageSum = 0;
     for(int i = 0; i < n; i++) {
+        // add the number of pages for student 1
         if(pageSum + arr[i] <= mid) {
             pageSum += arr[i];
         }
         else{
+            //change the student to student 2
             studentCount++;
+            //if students are finished
             if(studentCount > k || arr[i] > mid) {
                 return false;
             }
-            pageSum = 0;
+            //if students are left
+            pageSum = 0; //assign to student 2 and onwards
             pageSum += arr[i];
         }
     }
@@ -27,15 +33,20 @@ int splitArray(vector<int>& nums, int k) {
     for(int i = 0; i < n; i++) {
         sum += nums[i];
     }
+
+    // calculating solution space (s se lekar e tk)
     int s = 0, e = sum;
     int ans = -1;
     int mid = s+ (e-s)/2;
     while(s <= e) {
+        //check if mid is a possible solution
         if(isSolution(nums, n, mid ,k)) {
             ans = mid;
+            //if yes shift e to find minimum solution
             e = mid-1;
         }
         else {
+            // shift s to calculate a bigger value of mid
             s = mid+1;
         }
         mid = s+ (e-s)/2;
