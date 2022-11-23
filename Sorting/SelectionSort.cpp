2b4bdae99f762a1519f9e8d2d3f8a39dc007b66f
Your Name <you@example.com>
2022-11-23 19:00:40 +0530

    selection sort

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for(int j = i; j < n; j++) {
+                if(nums[j]<nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(nums[minIndex], nums[i]);
+        }
+        return nums;
+    }
+
+int main()
+{
+    vector<int> v{2,5,9,1,6,3};
+    vector<int> result;
+    result = sortArray(v);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout<<result[i]<<" ";
+    }
+    cout<<endl;
+    
+    return 0;
+}
